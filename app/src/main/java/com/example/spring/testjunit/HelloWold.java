2d8525153ba4da2371364ee5eb6703a5ccda06c8
chunhua.zeng@aispeech.com <chunhua.zeng@aispeech.com>
2017-05-03 16:34:00 +0800

    first commit

---
+++ app/src/main/java/com/example/spring/testjunit/HelloWold.java
@@ -0,0 +1,13 @@
+package com.example.spring.testjunit;
+
+/**
+ * Created by Spring on 17-5-3.
+ * to do:
+ */
+
+public class HelloWold {
+
+    public String printtest(String name) {
+        return name;
+    }
+}
